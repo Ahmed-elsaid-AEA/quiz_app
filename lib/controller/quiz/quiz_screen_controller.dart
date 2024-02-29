b9c8df2360144b65d954d2c492ead319d961bae2
Ahmed-elsaid-AEA <ahmed122727727@gmail.com>
2024-02-29 18:55:12 +0200

    make Custom List View Options Quiz Screen

---
+++ lib/controller/quiz/quiz_screen_controller.dart
@@ -0,0 +1,6 @@
+class QuizScreenController {
+  List<String> option = [
+    "afds","84","65","986523.0","dsdsdsd","sdds",
+    "afds","84","65","986523.0","dsdsdsd","sdds",
+  ];
+}
--- lib/view/quiz/screens/quiz_screen.dart
+++ lib/view/quiz/screens/quiz_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:quiz_app/controller/quiz/quiz_screen_controller.dart';
 import 'package:quiz_app/core/resources/color_manager.dart';
 import 'package:quiz_app/core/resources/font_managers.dart';
 import 'package:quiz_app/core/resources/height_manger.dart';
@@ -12,8 +13,22 @@ import 'package:quiz_app/view/quiz/widgets/custom_circle_percent_indictor_quiz_s
 import 'package:quiz_app/view/quiz/widgets/custom_item_radio_quiz_screen.dart';
 import 'package:quiz_app/view/quiz/widgets/custom_question_title_quiz_screen.dart';
 
-class QuizScreen extends StatelessWidget {
+class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  late QuizScreenController _quizScreenController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _quizScreenController = QuizScreenController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,41 +37,44 @@ class QuizScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding:
-            const EdgeInsets.symmetric(horizontal: PaddingValuesManagers.p24),
+        const EdgeInsets.symmetric(horizontal: PaddingValuesManagers.p24),
         child: Container(
           width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: HeightValuesManager.h88,
-              ),
-              Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  CustomQuestionTitleQuizScreen(
-                      text:
-                          "In what year did the United States host the FIFA World Cup for the first time?"),
-                  const Positioned(
-                    top: -29,
-                    right: 0,
-                    left: 0,
-                    child: CustomCirclePercentIndicatorQuizScreen(
-                      text: '30',
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: HeightValuesManager.h70,
-              ),
-              CustomItemRadioQuizScreen(),
-              CustomItemRadioQuizScreen(),
-              CustomItemRadioQuizScreen(),
-              CustomItemRadioQuizScreen(),
-              CustomItemRadioQuizScreen(),
-              CustomItemRadioQuizScreen(),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: HeightValuesManager.h88,
+                ),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    CustomQuestionTitleQuizScreen(
+                        text:
+                        "In what year did the United States host the FIFA World Cup for the first time?"),
+                    const Positioned(
+                      top: -29,
+                      right: 0,
+                      left: 0,
+                      child: CustomCirclePercentIndicatorQuizScreen(
+                        text: '30',
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: HeightValuesManager.h70,
+                ),
+                ListView.separated(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                    itemBuilder: (context, index) =>  CustomItemRadioQuizScreen(),
+                    separatorBuilder: (context, index) => SizedBox(height: 25,),
+                    itemCount:  _quizScreenController.option.length)
+
+              ],
+            ),
           ),
         ),
       ),
